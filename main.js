fc0b62ee44d5b4ae48325426cbe24e597fe8f663
karola-kasprzak <karola.a.kasprzak@gmail.com>
2021-01-07 18:21:19 +0100

    js logic added

--- main.js
+++ main.js
@@ -1,131 +1,158 @@
 const calc = {
-    inputString: "",
-    value1: number, //float number
-    value2: number, //float number
-    displayResult: number, //pushed to display - input will check for strings, if not then result will be cleared, limited to 10 characters
-    operatorTriggered: false,
+    // inputString: "",
+    // PO CO TO???
+    // displayResult: null,
+    //pushed to display - input will check for strings, if not then result will be cleared, limited to 10 characters
 
-    // operatorValue: {
-    //     none : 0,
-    //     oneByX : 1,
-    //     sqrt : 2,
-    //     sqr : 3,
-    //     divide : 4,
-    //     multiply : 5,
-    //     subtract : 6,
-    //     add : 7
-    // },
+    inputArr: [],
+    value1: null, //float number
+    value2: null, //float number
+    operatorTriggered: false,
+    operatorValue: null,
+    decPointTriggered: false,
+    maxLength: 10,
+
+    pushToDisplay(val) {
+        document.getElementById("calc-result").value = val;
+    },
+
+    // f. updating and displaying values
+    display(num) {
+        //adding input to array
+        this.inputArr.push(num);
+
+        //trimming input to 10 digits by limit input array lenght
+        if (this.inputArr.length > this.maxLength) {
+            this.inputArr = this.inputArr.slice(0, this.maxLength);
+        }
+
+        //converting input array to a number as variable currentNum
+        let currentNum = parseFloat(this.inputArr.join(""));
+
+        // storing currentNumber as value1 or value2
+        this.operatorTriggered
+            ? (this.value2 = currentNum)
+            : (this.value1 = currentNum);
+
+        // pushing current number to display
+        this.pushToDisplay(currentNum);
+        console.log(calc);
+    },
 
     //function clearing value strings
     clear() {
-        this.value1 = "";
-        this.value2 = "";
+        this.value1 = null;
+        this.value2 = null;
         this.operatorTriggered = false;
-        (this.displayResult = 0),
-            (this.inputString = ""),
-            (document.getElementById("calc-result").value = this.displayResult);
+        this.operatorValue = null;
+        this.decPointTriggered = false;
+        this.inputArr = [];
+        this.pushToDisplay(0);
+        console.log(calc);
     },
 
-    //function adding values to value1 and displaying them --- only here value1 and value2 are strings
-    display(num) {
-        if (this.displayResult != 0) {
-            this.inputString = `${num}`;
-            this.displayResult = this.inputString;
-            document.getElementById("calc-result").value = this.inputString;
-        } else if (this.operatorTriggered === false) {
-            this.value1 = this.value1 + num;
-            document.getElementById("calc-result").value = this.value1;
-            // console.log("display -> document.getElementById("calc-result").value", document.getElementById("calc-result").value);
-        } else {
-            document.getElementById("calc-result").value = "";
-            this.value2 = this.value2 + num;
-            document.getElementById("calc-result").value = this.value2;
+    //f. adding a decimal point to value
+    decPoint() {
+        if (!this.decPointTriggered) {
+            //check if there is an integer before decimal point
+            this.inputArr.length > 0
+                ? this.inputArr.push(".")
+                : this.inputArr.push("0.");
+            this.decPointTriggered = true;
         }
-    },
-
-    //function for single value operations
-    calculateSingle() {},
-
-    //function for double value operations
-    calculateDouble() {
-        let result = 0;
-
-        //condition added for doing operations with shorthand, where second value equals the first such as 4*=
-        if (this.value2 == "") {
-            this.value2 = this.value1;
-        }
-
-        switch (this.operatorValue) {
-            case "+":
-                result = parseFloat(this.value1) + parseFloat(this.value2);
-                break;
-            case "-":
-                result = parseFloat(this.value1) - parseFloat(this.value2);
-                break;
-            case "/":
-                result = parseFloat(this.value1) / parseFloat(this.value2);
-                break;
-            case "*":
-                result = parseFloat(this.value1) * parseFloat(this.value2);
-                break;
-
-            default:
-                document.getElementById("calc-result").value = "ERR";
-        }
-        this.displayResult11 = result.toString();
-        document.getElementById(
-            "calc-result"
-        ).value = this.displayResult11.substring(0, 11);
-        this.value1 = result;
-        this.value2 = "";
-        // this.operatorValue = ""; --needs to be commented to allow chain calculations on the same value--
-        this.operatorTriggered = false;
     },
 
     //function for triggering operators for double value operations
     trigger(val) {
+        //chaining operations if opperator is triggered the second time ex. 1 + 3 + 4
+        if (this.operatorTriggered) {
+            this.operate();
+        }
+
+        //changing bools and operator value
         this.operatorTriggered = true;
         this.operatorValue = val;
+        this.inputArr = [];
+
+        console.log(calc);
+    },
+
+    //f. calculating the result chosen mathematical operation
+    operate() {
+        switch (this.operatorValue) {
+            case "+":
+                result = this.value1 + this.value2;
+                break;
+            case "-":
+                result = this.value1 - this.value2;
+                break;
+            case "/":
+                result = this.value1 / this.value2;
+                break;
+            case "*":
+                result = this.value1 * this.value2;
+                break;
+            default:
+                document.getElementById("calc-result").value = "ERR";
+        }
+        this.clear();
+        this.value1 = result;
+        this.pushToDisplay(this.value1);
+        console.log(calc);
     },
 
     //function for finding square root of x
     sqrtX() {
-        this.value1 = Math.sqrt(this.value1);
-        this.displayResult11 = this.value1.toString();
-        document.getElementById(
-            "calc-result"
-        ).value = this.displayResult11.substring(0, 11);
+        //math operation
+        let currentNum = Math.sqrt(this.value1);
+
+        //clear values and bools
+        this.clear();
+
+        //set currentNum as value1
+        this.value1 = currentNum;
+
+        //trim currentNum if exceeds maxLength
+        currentNum.toString().length > this.maxLength
+            ? (currentNum = parseFloat(
+                  this.value1.toString().slice(0, this.maxLength)
+              ))
+            : null;
+
+        //display trimmed currentNum (value1 stays untrimmed!)
+        this.pushToDisplay(currentNum);
+
+        console.log(calc);
     },
 
     //function for squaring value1
     sqrX() {
-        this.value1 = Math.pow(this.value1, 2);
-        this.displayResult11 = this.value1.toString();
-        document.getElementById(
-            "calc-result"
-        ).value = this.displayResult11.substring(0, 11);
+        //math operation
+        let currentNum = Math.pow(this.value1, 2);
+
+        //clear values and bools
+        this.clear();
+
+        //set currentNum as value1
+        this.value1 = currentNum;
+
+        //trim currentNum if exceeds maxLength
+        currentNum.toString().length > this.maxLength
+            ? (currentNum = parseFloat(
+                  this.value1.toString().slice(0, this.maxLength)
+              ))
+            : null;
+
+        //display trimmed currentNum (value1 stays untrimmed!)
+        this.pushToDisplay(currentNum);
+
+        console.log(calc);
     },
 
-    //function converting number to 1/x
-    oneByX() {
-        if (this.operatorTriggered === false) {
-            this.value1 = 1 / parseFloat(this.value1);
-            this.displayResult11 = this.value1.toString();
-            document.getElementById(
-                "calc-result"
-            ).value = this.displayResult11.substring(0, 11);
-        } else {
-            this.value2 = 1 / parseFloat(this.value2);
-            this.displayResult11 = this.value2.toString();
-            document.getElementById(
-                "calc-result"
-            ).value = this.displayResult11.substring(0, 11);
-        }
-    },
-
-    //function calc-dot
-    dot() {
-        if (a > b) {
-        }
+    // DO ZROBIENIA
+    //f. backspace
+    backSpace() {
+        // this.inputArr.pop();
+        // console.log(calc);
     },
 };
